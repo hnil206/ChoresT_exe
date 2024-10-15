e53f05474c149bfc74865d6d3ddb43af3373ce7d
Pickky03 <vhai02092003@gmail.com>
2024-10-15 23:21:00 +0700

    13

--- client/next.config.js
+++ client/next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   images: {
     domains: ['www.vietnamworks.com', 'www.canva.com', 'marketplace.canva.com', 'cdn.pixabay.com', 'res.cloudinary.com'],
   },
+  output: 'public',
 }
 
 module.exports = nextConfig
