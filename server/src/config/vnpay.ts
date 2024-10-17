2475a2dafcbaec6bb85e9614b092a4e1a5527e8c
Pickky03 <vhai02092003@gmail.com>
2024-10-17 23:24:39 +0700

    mfix

--- server/src/config/vnpay.ts
+++ server/src/config/vnpay.ts
@@ -2,5 +2,5 @@ export const vnpayConfig = {
   vnp_TmnCode: "WXLB4BAH",
   vnp_HashSecret: "V8WKA4YUU7VAB4U2HEIZ5P39RTDZ92WF",
   vnp_Url: "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html",
-  vnp_ReturnUrl: "https://chores-t-exe.vercel.app/payment/vnpay_return",
+  vnp_ReturnUrl: "https://chorest-exe-2.onrender.com/payment/vnpay_return",
 };
--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { signup, login, logout, updateProfile, getProfile, getAllHousemaids} from '../controller/user.controller';
+import { signup, login, logout, updateProfile, getProfile, getAllHousemaids, getAllUsers} from '../controller/user.controller';
 import { verifyToken } from '../middlewares';
 
 const router = express.Router();
@@ -10,6 +10,7 @@ router.post('/signup', signup);
 // POST /auth/login
 router.post('/login', login);
 router.post('/logout', logout);
+router.get('/users', getAllUsers);
 router.get('/housemaid', getAllHousemaids);
 router.put('/user/update', verifyToken,updateProfile);
 router.get('/user/profile', verifyToken, getProfile);
