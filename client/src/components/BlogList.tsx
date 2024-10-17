519289ae044d03916f1e40a42d4ac7f81d01e1f8
Pickky03 <vhai02092003@gmail.com>
2024-10-17 22:03:32 +0700

    add consle

--- client/src/components/BlogList.tsx
+++ client/src/components/BlogList.tsx
@@ -23,6 +23,7 @@ export default function BlogList() {
     const fetchBlogs = async () => {
         try {
             const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/blogs`);
+            console.log(response.data)
             setBlogs(response.data);
         } catch (error) {
             console.error('Error fetching blogs:', error);
