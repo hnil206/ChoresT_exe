3704b340605c34bb37df9cd014c43523be6a331b
Pickky03 <vhai02092003@gmail.com>
2024-10-22 14:33:28 +0700

    1

--- client/src/components/header-wrapper.tsx
+++ client/src/components/header-wrapper.tsx
@@ -1,13 +1,12 @@
 'use client'
 import useAuth from "@/app/hook/useAuth";
 import Header from "./app.header";
-import useAuthToken from "@/app/hook/useAuth";
 
 export default function HeaderWrapper() {
-    const { isAuthenticated } = useAuth();
+    const { isAuthenticated, isAdmin } = useAuth();
     return (
         <div>
-            <Header isAuthenticated={isAuthenticated} />
+            <Header isAuthenticated={isAuthenticated} isAdmin={isAdmin} />
         </div>
     )
 }
